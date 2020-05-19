07d3069ab870bbfbe0a0de8004717b19d6c5754e
Thanos Panagiotidis <thanospan@users.noreply.github.com>
2020-05-19 18:08:18 +0300

    Add BatchOperations, BatchOperationsExample

---
+++ src/main/java/com/thanospan/ngsi/v2/client/api/BatchOperations.java
@@ -0,0 +1,50 @@
+package com.thanospan.ngsi.v2.client.api;
+
+import com.thanospan.ngsi.v2.client.ApiClient;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+
+public class BatchOperations {
+    private ApiClient apiClient;
+    private HttpClient httpClient;
+
+    public BatchOperations(ApiClient apiClient) {
+        this.apiClient = apiClient;
+        this.httpClient = apiClient.getHttpClient();
+    }
+
+    public CompletableFuture<HttpResponse<String>> update(String contentType, String body, HashMap<String, String> parameters) {
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(apiClient.buildUri("/op/update", parameters)))
+                .header("Content-Type", contentType)
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+
+        return httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public CompletableFuture<HttpResponse<String>> query(String contentType, String body, HashMap<String, String> parameters) {
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(apiClient.buildUri("/op/query", parameters)))
+                .header("Content-Type", contentType)
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+
+        return httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public CompletableFuture<HttpResponse<String>> notify(String contentType, String body, HashMap<String, String> parameters) {
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(apiClient.buildUri("/op/notify", parameters)))
+                .header("Content-Type", contentType)
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+
+        return httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString());
+    }
+}
---
+++ src/main/java/com/thanospan/ngsi/v2/client/examples/BatchOperationsExample.java
@@ -0,0 +1,55 @@
+package com.thanospan.ngsi.v2.client.examples;
+
+import com.thanospan.ngsi.v2.client.ApiClient;
+import com.thanospan.ngsi.v2.client.api.BatchOperations;
+
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+
+public class BatchOperationsExample {
+    public static void main(String[] args) {
+        ApiClient apiClient = new ApiClient();
+        BatchOperations batchOperations = new BatchOperations(apiClient);
+
+        update(batchOperations);
+//        query(batchOperations);
+//        notify(batchOperations);
+    }
+
+    private static void update(BatchOperations batchOperations) {
+        HashMap<String, String> parameters = new HashMap<>();
+//        parameters.put("options", "keyValues");
+        String contentType = "application/json";
+        String body = "{\"actionType\":\"append\",\"entities\":[{\"type\":\"Room\",\"id\":\"Bcn-Welt\",\"temperature\":{\"value\":21.7},\"humidity\":{\"value\":60}},{\"type\":\"Room\",\"id\":\"Mad_Aud\",\"temperature\":{\"value\":22.9},\"humidity\":{\"value\":85}}]}";
+        HttpResponse<String> httpResponse = batchOperations.update(contentType, body, parameters).join();
+        printHttpResponse(httpResponse);
+    }
+
+    private static void query(BatchOperations batchOperations) {
+        HashMap<String, String> parameters = new HashMap<>();
+//        parameters.put("limit", "10");
+//        parameters.put("offset", "20");
+//        parameters.put("orderBy", "temperature,!speed");
+//        parameters.put("options", "count");
+        String contentType = "application/json";
+        String body = "{\"entities\":[{\"idPattern\":\".*\",\"type\":\"Room\"},{\"id\":\"Car\",\"type\":\"P-9873-K\"}],\"attrs\":[\"temperature\",\"humidity\"],\"expression\":{\"q\":\"temperature>20\"},\"metadata\":[\"accuracy\",\"timestamp\"]}";
+        HttpResponse<String> httpResponse = batchOperations.query(contentType, body, parameters).join();
+        printHttpResponse(httpResponse);
+    }
+
+    private static void notify(BatchOperations batchOperations) {
+        HashMap<String, String> parameters = new HashMap<>();
+//        parameters.put("options", "keyValues");
+        String contentType = "application/json";
+        String body = "{\"subscriptionId\":\"5aeb0ee97d4ef10a12a0262f\",\"data\":[{\"type\":\"Room\",\"id\":\"DC_S1-D41\",\"temperature\":{\"value\":35.6,\"type\":\"Number\"}},{\"type\":\"Room\",\"id\":\"Boe-Idearium\",\"temperature\":{\"value\":22.5,\"type\":\"Number\"}}]}";
+        HttpResponse<String> httpResponse = batchOperations.notify(contentType, body, parameters).join();
+        printHttpResponse(httpResponse);
+    }
+
+    private static void printHttpResponse(HttpResponse<String> httpResponse) {
+        System.out.println(httpResponse.request());
+        System.out.println(httpResponse.statusCode());
+        System.out.println(httpResponse.headers().map());
+        System.out.println(httpResponse.body());
+    }
+}
